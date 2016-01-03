c18a63859ac1db13d7ddceab65a740832cfe5d5c
JP Slavinsky <jpslav@gmail.com>
2016-01-03 14:26:09 -0800

    queries now run in one redis call\!

--- lib/jobba/query.rb
+++ lib/jobba/query.rb
@@ -15,7 +15,7 @@ class Jobba::Query
   end
 
   def count
-    run_with_block(&COUNT_STATUSES)
+    _run(COUNT_STATUSES)
   end
 
   def empty?
@@ -39,7 +39,7 @@ class Jobba::Query
   end
 
   def run
-    run_with_block(&GET_STATUSES)
+    _run(GET_STATUSES)
   end
 
   protected
@@ -50,43 +50,73 @@ class Jobba::Query
     @clauses = []
   end
 
-  GET_STATUSES = ->(working_set) {
-    ids = Jobba.redis.zrange(working_set, 0, -1)
-    Jobba::Statuses.new(ids)
-  }
+  class RunBlocks
+    attr_reader :redis_block, :output_block
 
-  COUNT_STATUSES = ->(working_set) {
-    Jobba.redis.zcard(working_set)
-  }
-
-  def run_with_block(&working_set_block)
-
-    # TODO I think this code can be rewritten to send all calls to redis
-    # in one or maybe two `multi` blocks.  A complication comes in
-    # how the GET_STATUSES block uses the result of a redis call in
-    # its instantiation of a Statuses object.  That would have to
-    # be split apart somehow -- like maybe this method can take two blocks
-    # -- one that is run inside the multi block, and another that runs
-    # outside of the multi block taking as input the output of the first
-    # block.
-
-    load_default_clause if clauses.empty?
-    working_set = nil
-
-    clauses.each_with_index do |clause, ii|
-      clause_set = clause.to_new_set
-
-      if working_set.nil?
-        working_set = clause_set
-      else
-        redis.zinterstore(working_set, [working_set, clause_set], weights: [0, 0])
-        redis.del(clause_set)
-      end
+    def initialize(redis_block, output_block)
+      @redis_block = redis_block
+      @output_block = output_block
     end
 
-    working_set_block.call(working_set).tap do
+    def output_block_result_is_redis_block_result?
+      output_block.nil?
+    end
+  end
+
+  GET_STATUSES = RunBlocks.new(
+    ->(working_set, redis) {
+      redis.zrange(working_set, 0, -1)
+    },
+    ->(ids) {
+      Jobba::Statuses.new(ids)
+    }
+  )
+
+  COUNT_STATUSES = RunBlocks.new(
+    ->(working_set, redis) {
+      redis.zcard(working_set)
+    },
+    nil
+  )
+
+  def _run(run_blocks)
+    # Each clause in a query is converted to a sorted set (which may be filtered,
+    # e.g. in the case of timestamp clauses) and then the sets are successively
+    # intersected.
+    #
+    # Different users of this method have different uses for the final "working"
+    # set.  Because we want to bundle all of the creations and intersections of
+    # clause sets into one call to Redis (via a `multi` block), we have users
+    # of this method provide a final block to run on the working set within
+    # Redis (and within the `multi` call) and then another block to run on
+    # the output of the first block.
+
+    multi_result = redis.multi do
+      load_default_clause if clauses.empty?
+      working_set = nil
+
+      clauses.each_with_index do |clause, ii|
+        clause_set = clause.to_new_set
+
+        if working_set.nil?
+          working_set = clause_set
+        else
+          redis.zinterstore(working_set, [working_set, clause_set], weights: [0, 0])
+          redis.del(clause_set)
+        end
+      end
+
+      # This is later accessed as `multi_result[-2]` since it is the second to last output
+      run_blocks.redis_block.call(working_set, redis)
+
       redis.del(working_set)
     end
+
+    redis_block_output = multi_result[-2]
+
+    run_blocks.output_block_result_is_redis_block_result? ?
+      redis_block_output :
+      run_blocks.output_block.call(redis_block_output)
   end
 
   def load_default_clause
